f7d4da1792c7e2408f4ad35beaf8c0f4c9fecea1
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-02-21 22:18:40 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,4 @@
-/opt/gsnova/gsnova_paas_linux_amd64 -key $KEY -listen :8080
+cd /opt/gsnova
+curl -o cert.pem $CERT_FILE
+curl -o key.pem $KEY_FILE
+./gsnova_paas_linux_amd64 -key $KEY -listen :8080
