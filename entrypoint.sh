915ac85493b7e6c39f3daf8cc5e7085f2e962110
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-02-22 19:44:05 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
-echo '$CERT_PEM' |  tee /opt/gsnova/cert.pem
-echo '$KEY_PEM'  |  tee /opt/gsnova/key.pem
+if [$CERT_PEM !='none' && $KEY_PEM !='none']; then
+  echo $CERT_PEM |  tee /opt/gsnova/cert.pem
+  echo $KEY_PEM  |  tee /opt/gsnova/key.pem
+fi
 /opt/gsnova/gsnova_paas_linux_amd64 -key $KEY -listen :8080
