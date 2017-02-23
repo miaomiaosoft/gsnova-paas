3e61efe9002b861e6c925a2eef34db136db68e8c
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-02-23 18:10:09 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-  echo $CERT_PEM > /opt/gsnova/cert.pem
-  echo $KEY_PEM  > /opt/gsnova/key.pem
+  echo -e "$CERT_PEM" > /opt/gsnova/cert.pem
+  echo -e "$KEY_PEM"  > /opt/gsnova/key.pem
 fi
 /opt/gsnova/gsnova_paas_linux_amd64 -key $KEY -listen :8080
